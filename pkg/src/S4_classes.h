993e0bec6e6152097bb7aee6f9858d847cfdc26e
edd <edd@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-01-11 15:05:10 +0000

    added THANKS file (and logged in ChangeLog) added / updated copyright headers for a few source files
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rprotobuf@409 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/S4_classes.h
+++ pkg/src/S4_classes.h
@@ -1,8 +1,8 @@
 // -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; tab-width: 8 -*-
 //
-// rprotobuf.h: R/C++ interface class library
+// S4_classes.h: R/C++ interface class library
 //
-// Copyright (C) 2010 Dirk Eddelbuettel and Romain Francois
+// Copyright (C) 2010 - 2011  Dirk Eddelbuettel and Romain Francois
 //
 // This file is part of RProtoBuf.
 //
