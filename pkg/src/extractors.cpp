132441ab76e2ceba6427dfed8cb67b32baac91c3
edd <edd@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-04-10 17:30:59 +0000

    added standard header and change one indentation
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rprotobuf@418 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/extractors.cpp
+++ pkg/src/extractors.cpp
@@ -1,3 +1,23 @@
+// -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; tab-width: 4 -*-
+/* :tabSize=4:indentSize=4:noTabs=false:folding=explicit:collapseFolds=1: */
+//
+// Copyright (C) 2010 Dirk Eddelbuettel and Romain Francois
+//
+// This file is part of RProtoBuf.
+//
+// RProtoBuf is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// RProtoBuf is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with RProtoBuf.  If not, see <http://www.gnu.org/licenses/>.
+
 #include "rprotobuf.h"
 #include "fieldtypes.h"
 #include "Rcppsupport.h"
@@ -48,8 +68,8 @@ SEXP extractFieldAsSEXP( const Rcpp::XPtr<GPB::Message>& message, const GPB::Des
 
 #undef HANDLE_REPEATED_FIELD
 #define HANDLE_REPEATED_FIELD(TYPE,DATATYPE) \
-	case TYPE : \
-		return Rcpp::wrap( RepeatedFieldImporter<DATATYPE>(ref, *message, fieldDesc) ) ; \
+		case TYPE :													\
+			return Rcpp::wrap( RepeatedFieldImporter<DATATYPE>(ref, *message, fieldDesc) ) ; \
 
 			HANDLE_REPEATED_FIELD(CPPTYPE_INT32, GPB::int32) ;
     		HANDLE_REPEATED_FIELD(CPPTYPE_INT64, GPB::int64) ;
