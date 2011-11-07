47ad7e4762ea60bf8de5ce565a7b7f15eae5783c
romain <romain@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-11-07 14:08:38 +0000

    testing int64 support in message fields
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rprotobuf@432 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/inst/unitTests/runit.int64.support.R
@@ -0,0 +1,31 @@
+# Copyright 2011 Google Inc.
+# Copyright 2011 Dirk Eddelbuettel and Romain Francois 
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+.setUp <- function(){
+    if( ! exists( "TestInt64", "RProtoBuf:DescriptorPool" ) ){
+        unitest.proto.file <- system.file( "unitTests", "data", "int64.proto", package = "RProtoBuf" )
+        readProtoFiles(file = unitest.proto.file)
+    }
+}
+
+test.int64 <- function() {
+  msg <- new( TestInt64, a = 1, b = 2, c = 1:4, d = 1:4 )
+  checkEquals( msg$a, as.int64(1) )
+  checkEquals( msg$b, as.uint64(2) )
+  checkEquals( msg$c, as.int64(1:4) )
+  checkEquals( msg$d, as.uint64(1:4) )
+}
